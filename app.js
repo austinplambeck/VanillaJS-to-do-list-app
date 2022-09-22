b4e05155336863fa781447f5a296805afeb1c883
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-09-22 16:39:27 -0700

    comm2 - 9/22 progress

---
+++ app.js
@@ -0,0 +1,90 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
+// Functions
+
+// ADDS TODO ITEM TO THE LIST
+function addTodo(event) {
+    // PREVENT FORM FROM SUBMITTING
+    event.preventDefault(); 
+    // TODO DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // CREATE LI (NEW To-Do Item)
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    // CLEAR TODO INPUT VALUE
+    todoInput.value = '';
+}
+
+// REMOVES A TODO AND COMPLETES A TODO
+function deleteCheck(e) {
+    const item = e.target;
+    // DELETE TODO
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+    // CHECK OFF TODO
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+// FILTERS TODO ITEMS INTO DIFFERENT SELECTED CATGEGORIES
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
+}
+
+// SAVES TODO LIST TO LOCAL STORAGE!
+function saveLocalTodos(todo) {
+
+}
