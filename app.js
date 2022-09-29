7bd52ebf92b61f1f7b90599bcda65a850783926b
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-09-28 18:01:11 -0700

    comm3 - finished project

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
@@ -23,6 +24,8 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    // ADD TODO TO LOCAL STORAGE
+    saveLocalTodos(todoInput.value);
     // CHECK MARK BUTTON
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -47,6 +50,7 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         // Animation
         todo.classList.add('fall');
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function(){
             todo.remove();
         });
@@ -86,5 +90,69 @@ function filterTodo(e) {
 
 // SAVES TODO LIST TO LOCAL STORAGE!
 function saveLocalTodos(todo) {
-
+    // CHECK -- Do I already have items in local storage?
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    // Passing in todo into the todos array
+    todos.push(todo);
+    // Pushing back into local storage
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+// Retrieving any todos from local storage and showing them in the list
+function getTodos() {
+    // CHECK -- Do I already have items in local storage?
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo) {
+        // TODO DIV
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        // CREATE LI (NEW To-Do Item)
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+        // CHECK MARK BUTTON
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add('complete-btn');
+        todoDiv.appendChild(completedButton);
+        // TRASH BUTTON
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);
+        // APPEND TO LIST
+        todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo) {
+    // CHECK -- Do I already have items in local storage?
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    // Use the .splice method to remove certain elements from the array
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// .SPLICE EXAMPLE
+
+// const todos = ['apple', 'john', 'donut'];
+// const johnIndex = todos.indexOf('john');
+// todos.splice(johnIndex, 1);
+// console.log(todos); -- THIS WILL PRINT ARRAY WITH JOHN REMOVED
